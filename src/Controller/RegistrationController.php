90129c20f11449e6f8025ed014361c695b1907d8
JennyViannay <jenny.viannay75@gmail.com>
2020-07-02 16:11:41 +0200

    fix

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -40,7 +40,7 @@ class RegistrationController extends AbstractController
                     $form->get('plainPassword')->getData()
                 )
             );
-
+            $user->setRoles($user->getRoles());
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
             $entityManager->flush();
